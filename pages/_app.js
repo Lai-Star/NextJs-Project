4747397a1ac1f15c4bc368a3ea26843f8f4f4716
andrews9901018 <andrews990101@outlook.com>
2021-06-21 23:22:46 +0300

    Add theme styles & font-awesome css

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
-import '@styles/globals.css'
+import "@styles/globals.css";
+import "@styles/css/nucleo-icons.css";
+import "@styles/scss/blk-design-system-react.scss?v=1.2.0";
+import "@fortawesome/fontawesome-free/css/all.css";
+import "@fortawesome/fontawesome-free/js/all.js";
 
 function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default Application
+export default Application;
