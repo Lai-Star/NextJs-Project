cc8c99e620262f4c3c22037cd33285402dec3646
andrews9901018 <andrews990101@outlook.com>
2021-06-21 23:21:01 +0300

    Change index.js

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,22 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
+import Footer from "@components/Footer";
+import Profile from "@components/Profile";
+import Navbars from "@components/Navbars";
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Ahmet Buğra Aydın</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        {/* <Navbars /> */}
+        <Profile />
       </main>
 
       <Footer />
     </div>
-  )
+  );
 }
